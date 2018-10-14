e815da709661e9559a1a56996f05803cc4e8946f
unknown <juliojgd@users.noreply.github.com>
2018-10-14 20:54:50 +0200

    Initial commit

---
+++ src/main/kotlin/juliojgd/kotlin/anmetrigal/AnMetriGalApplication.kt
@@ -0,0 +1,11 @@
+package juliojgd.kotlin.anmetrigal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnMetriGalApplication
+
+fun main(args: Array<String>) {
+    runApplication<AnMetriGalApplication>(*args)
+}
