04b9cf305b30510fcf905e7cb732d0a85fd9ff06
Julio Gomez Diaz <juliojgd@gmail.com>
2020-02-29 18:12:16 +0100

    Builds again with latest Spring Boot 2.2.5 Release and Kotlin 1.3.61

--- src/main/kotlin/io/github/juliojgd/anmetrigalkotlin/config/AnMetriGalKotlinConfiguration.kt
+++ src/main/kotlin/io/github/juliojgd/anmetrigalkotlin/config/AnMetriGalKotlinConfiguration.kt
@@ -1,17 +1,10 @@
 package io.github.juliojgd.anmetrigalkotlin.config
 
-import io.github.juliojgd.anmetrigalkotlin.domain.structure.EstructuraEstrofa
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
-import javax.annotation.PostConstruct
 
 
 @Configuration
 class AnMetriGalKotlinConfiguration /*(val props:AnMetriGalProperties)*/ {
-
 
 
 //    @PostConstruct
